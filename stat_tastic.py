ae7e93a269f3955b10f6eac930cd66a9a66674be
Minho Cho <mc811mc@gmail.com>
2020-08-25 19:50:13 +0900

    Create stat_tastic.py
    Rudimentary

---
+++ stat_tastic.py
@@ -0,0 +1,25 @@
+DATA = 'sample_dataset'
+with open(DATA) as file:
+    for line in file:
+        pass
+
+def total():
+    pass
+def summation(DATA):
+    sum = 0
+    for x in DATA:
+        sum += x
+    return sum
+
+def mean():
+    pass
+def std_dev():
+    pass
+def median():
+    pass
+def minimum():
+    pass
+def maximum():
+    pass
+
+summation(DATA)
