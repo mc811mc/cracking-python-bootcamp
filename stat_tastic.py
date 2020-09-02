c2ef9787c2af197bb4c9e73f703b763999141a08
Minho Cho <mc811mc@gmail.com>
2020-09-02 16:51:25 +0900

    Update stat_tastic.py

--- stat_tastic.py
+++ stat_tastic.py
@@ -1,9 +1,5 @@
-#import data
-data = 'sample_dataset'
-with open(data) as file:
-    for line in file:
-        pass
-    
+from math import sqrt
+
 #total 
 def total():
     return (len(data))
@@ -16,7 +12,7 @@ def summation():
         x += 1
     return sum
 
-#mean (average)
+#mean
 def mean():
     sum = 0
     for x in data:
@@ -25,17 +21,55 @@ def mean():
     average = sum / len(data)
     return average
 
-#standard deviation
-def std():
-    sum = 0
-    for x in data:
-        sum += x
+#variance
+def variance():
+    for x in range(0, len(data)):
+        sum = (data[x] - mean())**2
+        sum += sum
         x += 1
-    average = sum / len(data)
-    for x in data:
-        diff = x - average
-        sqt  = diff ** 2
-        std += sqt / len(data)
-    return std
+    return sum / (total() - 1)
 
+#standard deviation
+def deviation():
+    deviation = variance()
+    return sqrt(deviation)
 
+#median
+def median():
+    if len(data) % 2 == 0:
+        placement = int(len(data) / 2)
+        median = (data[placement] + data[placement - 1]) / 2
+        return median 
+
+    if len(data) % 2 == 1:
+        placement = int((len(data) - 1) / 2)
+        median = data[placement]
+        return median
+
+#minimum
+def minimum():
+    minimum = data[0]
+    for x in range(0, len(data)):
+        if data[x] < minimum:
+            minimum = data[x]
+            x += 1
+    return minimum
+
+#maximum
+def maximum():
+    maximum = data[0]
+    for x in range(0, len(data)):
+        if data[x] > maximum:
+            maximum = data[x]
+            x += 1
+    return maximum
+
+print("There are a total of", total(), "numbers")
+print("The sum is",summation())
+print("The average is", mean())
+print("The variance is", variance())
+print("The standard deviation is", deviation())
+print("The median is", median())
+print("The minimum value is", minimum())
+print("The maximum value is", maximum())
+    
