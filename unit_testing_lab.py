39dc7b51f70895d3abdb10bb9e8dec99fd34fafe
Minho Cho <mc811mc@gmail.com>
2020-09-11 23:40:47 +0900

    Update unit_testing_lab.py

---
+++ unit_testing_lab.py
@@ -0,0 +1,65 @@
+import unittest
+
+class UnitTesting(unittest.TestCase):
+
+    def test_abs(self):
+        self.assertEqual(100, abs(-100))
+
+    def test_bin(self):
+        self.assertEqual('01101000', bin(h))
+
+    def test_bool(self):
+        self.assertTrue(True)
+
+    # Return False
+    def test_callable(self):
+        x = 5
+        self.assertTrue(True, callable(x))
+
+    def test_chr(self):
+        self.assertEqual(A, chr(65))
+
+    def test_dict(self):
+        self.assert(, dict())
+
+    def binary(self):
+        self.assert(, divmod())
+    
+    def binary(self):
+        self.assert(, enumerate())
+
+    def binary(self):
+        self.assert(, float())
+
+    def binary(self):
+        self.assert(, hex())
+
+    def binary(self):
+        self.assert(, int())
+
+    def binary(self):
+        self.assert(, len())
+
+    def binary(self):
+        self.assert(, list())
+
+    def binary(self):
+        self.assert(, max())
+    
+    def binary(self):
+        self.assert(, min())
+
+    def binary(self):
+        self.assert(, next())
+
+    def binary(self):
+        self.assert(, oct())
+
+    def binary(self):
+        self.assert(, sorted())
+
+    def binary(self):
+        self.assert(, sum())
+
+if __name__ == '__main__':
+    unit_testing_lab.main()
